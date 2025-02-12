7e72abdf67b8ecbd2140061ab57720e1ea768d1a
Pasco Romane <romane.pasco@esaat-roubaix.com>
2025-02-12 21:11:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', function() {
   document.querySelector('h1').style.top = '20px';
 });
+
+const elements = document.querySelectorAll('.clickable');
+        const tooltip = document.getElementById('tooltip');
+
+        elements.forEach(element => {
+            element.addEventListener('mouseenter', function() {
+                const name = this.getAttribute('data-name');
+                tooltip.textContent = name;
+                tooltip.style.display = 'block';
+            });
+
+            element.addEventListener('mousemove', function(event) {
+                tooltip.style.left = event.pageX + 10 + 'px'; // Positionne le tooltip légèrement à droite du curseur
+                tooltip.style.top = event.pageY + 10 + 'px';  // Positionne le tooltip légèrement en dessous du curseur
+            });
+
+            element.addEventListener('mouseleave', function() {
+                tooltip.style.display = 'none'; // Cache le tooltip quand le curseur quitte l'élément
+            });
+        });
